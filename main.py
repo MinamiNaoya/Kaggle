6e01ded3c84643f48b400459cd960f99c0fd4066
NaoyaMinami <vm21111@st.kitasato-u.ac.jp>
2022-01-15 21:13:44 +0900

    データフレームの作成

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ test = pd.read_csv("C:\\Users\\owner\\Downloads\\test.csv")
 
 test_shape = test.shape
 train_shape = train.shape
-print(test.describe())
-print(train.describe())
+
+
+def kesson_table(df):
+    null_val = df.isnull().sum(df)
+    percent = 100 * df.isnull().sum()/len(df)
+    kesson_table=pd.concat([null_val, percent], axis=1)
+    kesson_table_ren_columns = kesson_table.rename(columns = {0: '欠損数', 1: '%'}
+    )
+
+    return kesson_table_ren_columns
+
+de
+
